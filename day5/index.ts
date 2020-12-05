ea972bbc868c9fbfada2acb6b4e6cdaebcc6e24c
Jan Tuomi <jans.tuomi@gmail.com>
2020-12-05 13:28:36 +0200

    Solve 5.2

--- day5/index.ts
+++ day5/index.ts
@@ -1,5 +1,5 @@
 import { Observable, of } from "rxjs";
-import { concatAll, count, filter, groupBy, map, max, mergeMap, reduce, tap } from 'rxjs/operators';
+import { concatAll, count, filter, groupBy, map, max, mergeMap, reduce, scan, tap, toArray } from 'rxjs/operators';
 import { ExerciseModuleFunc } from "../types";
 
 type Pair = [number, number];
@@ -10,8 +10,6 @@ const getColumnAndRow = (line: string): Pair => {
 
   lo = 0; hi = 127;
   for (let i = 0; i < 7; i += 1) {
-    console.log("i:",i,"lo:",lo,"hi:",hi);
-
     const chr = chars[i];
     const diff = hi - lo;
     if (chr === "F") {
@@ -26,8 +24,6 @@ const getColumnAndRow = (line: string): Pair => {
 
   lo = 0; hi = 7;
   for (let i = 0; i < 3; i += 1) {
-    console.log("i:",i,"lo:",lo,"hi:",hi);
-
     const chr = chars[i + 7];
     const diff = hi - lo;
     if (chr === "L") {
@@ -42,6 +38,31 @@ const getColumnAndRow = (line: string): Pair => {
   return [col, row];
 }
 
+const findVacantSeats = (lst: Pair[]): Pair[] => {
+  const seatMap: boolean[][] = Array(128);
+  for (let i = 0; i < 127; i += 1) {
+    seatMap[i] = Array(8);
+    for (let j = 0; j < 7; j += 1) {
+      seatMap[i][j] = true;
+    }
+  }
+
+  for (let seat of lst) {
+    seatMap[seat[1]][seat[0]] = false;
+  }
+
+  const vacants: Pair[] = [];
+  for (let row = 0; row < 127; row += 1) {
+    for (let col = 0; col < 7; col += 1) {
+      if (seatMap[row][col] === true) {
+        vacants.push([col, row]);
+      }
+    }
+  }
+
+  return vacants;
+}
+
 const calcID = (pair: Pair) => pair[1] * 8 + pair[0];
 
 const day5: ExerciseModuleFunc = async (input: string) => {
@@ -53,7 +74,18 @@ const day5: ExerciseModuleFunc = async (input: string) => {
     max()
   ).toPromise();
 
-  return Promise.all([prom1]);
+  const prom2 = of(input).pipe(
+    map(l => l.split("\n")),
+    concatAll(),
+    map(getColumnAndRow),
+    toArray(),
+    map(findVacantSeats),
+    concatAll(),
+    map(calcID),
+    toArray()
+  ).toPromise();
+
+  return Promise.all([prom1, prom2]);
 }
 
 export default day5;
